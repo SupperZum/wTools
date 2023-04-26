f1797fcab36cfc53d298fa97c5c2f0b01857106b
SupperZum <levchenko.d25@gmail.com>
2023-04-26 17:58:53 +0300

    commit

--- rust/test/test/test_tools_tests.rs
+++
@@ -1,9 +0,0 @@
-#![ warn( rust_2018_idioms ) ]
-#![ warn( missing_debug_implementations ) ]
-#![ warn( missing_docs ) ]
-
-use test_tools as TheModule;
-#[ allow( unused_imports ) ]
-use test_tools::exposed::*;
-
-mod inc;
